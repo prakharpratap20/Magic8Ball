d04ee50e496b5f70955d1dc1d6e2e8d7647ddaa4
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-09-04 14:26:21 +0530

    Initial Commit

---
+++ Magic8ball/Magic8ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8ballApp.swift
+//  Magic8ball
+//
+//  Created by Prakhar Pratap on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Magic8ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
